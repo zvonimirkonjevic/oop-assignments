92a5d32ec2ab126f3ac9a8e992ed00611e53eb5e
Zvonimir <zvonimir.konjevic@gmail.com>
2022-10-24 15:29:10 +0200

    Dodaj direktoriji OOP_ZAD1 sa prvom zadacom
    Dodao direktoriji OOP_ZAD1 sa prvom zadacom.

---
+++ OOP_ZAD1/OOP_ZAD1_ClassLibrary/Episode.cs
@@ -0,0 +1,47 @@
+﻿namespace OOP_ZAD1_ClassLibrary;
+
+public class Episode
+{
+    private int viewerCount;
+    private double scoreSum;
+    private double maxScore;
+
+    public Episode()
+    {
+        this.viewerCount = 0;
+        this.scoreSum = 0.0;
+        this.maxScore = 0.0;
+    }
+
+    public Episode(int viewerCount, double scoreSum, double maxScore)
+    {
+        this.viewerCount = viewerCount;
+        this.scoreSum = scoreSum;
+        this.maxScore = maxScore;
+    }
+
+    public void AddView(double score)
+    {
+        viewerCount++;
+        scoreSum += score;
+        if (maxScore < score)
+        {
+            maxScore = score;
+        }
+    }
+
+    public double GetMaxScore()
+    {
+        return this.maxScore;
+    }
+
+    public double GetAverageScore()
+    {
+        return (this.scoreSum / this.viewerCount);
+    }
+
+    public int GetViewerCount()
+    {
+        return this.viewerCount;
+    }
+}
---
+++ OOP_ZAD1/OOP_ZAD1_ConsoleApp/ConsoleApp.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOP_ZAD1_ClassLibrary;
+
+namespace OOP_ZAD1_ConsoleApp
+{
+    class Program
+    {
+        public static double GenerateRandomScore()
+        {
+            Random generator;
+            generator = new Random();
+            return generator.NextDouble() * (10.0 - 0.0) + 0.0;
+        }
+        
+        static void Main(string[] args)
+        {
+            Episode ep1, ep2;
+            ep1 = new Episode();
+            ep2 = new Episode(10, 64.39, 8.7);
+            int viewers = 10;
+            for (int i = 0; i < viewers; i++)
+            {
+                ep1.AddView(GenerateRandomScore());
+                Console.WriteLine(ep1.GetMaxScore());
+            }
+
+            if (ep1.GetAverageScore() > ep2.GetAverageScore())
+            {
+                Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
+            }
+            else
+            {
+                Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
+            }
+        }
+    }
+}
